bfdc8224a0fa7fdfc5c48f32a4c33ad2267655d1
hell-llex <demeshenko333@gmail.com>
2024-04-30 05:02:16 +0300

    feat: add film guide slice and types

--- src/store/slice/filmGuide.ts
+++ src/store/slice/filmGuide.ts
@@ -1,16 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Movie, SortingNavLinks } from '../../types';
 
 const initialState: {
-  items: [];
+  items: Movie[];
+  lastViewedItems: Movie[];
+  navItems: SortingNavLinks[],
 } = {
   items: [],
+  lastViewedItems: [],
+  navItems: [
+    { name: 'Popular', link: 'popular' },
+    { name: 'Now Playing', link: 'now_playing'},
+    { name: 'Top Rated', link: 'top_rated' },
+    { name: 'Upcoming', link: 'upcoming' },
+    { name: 'Last Viewed', link: 'last_viewed' }
+  ]
 };
 
 const filmGuideSlice = createSlice({
   name: 'filmGuide',
   initialState,
-  reducers: {},
+  reducers: {
+    changeItems(state, action: PayloadAction<Movie[]>) {
+      state.items = action.payload
+    },
+    addItemsLastViewed(state, action: PayloadAction<Movie>) {
+      const newItem = action.payload;
+      const { lastViewedItems } = state;
+      const existingIndex = lastViewedItems.findIndex(item => item.id === newItem.id);
+      if (existingIndex === -1) {
+        if (lastViewedItems.length < 20) {
+          state.lastViewedItems.push(newItem);
+        } else {
+          state.lastViewedItems.shift();
+          state.lastViewedItems.push(newItem);
+        }
+      } else {
+        lastViewedItems.splice(existingIndex, 1);
+        state.lastViewedItems = [...lastViewedItems, newItem];
+      }
+    }
+    
+  },
 });
 
 export const filmGuideReducer = filmGuideSlice.reducer;
-export const { } = filmGuideSlice.actions;
+export const { changeItems, addItemsLastViewed } = filmGuideSlice.actions;
---
+++ src/types/index.ts
@@ -0,0 +1,50 @@
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+export interface SortingNavLinks {
+  name: 'Popular' | 'Now Playing' | 'Top Rated' | 'Upcoming' | 'Last Viewed';
+  link: 'popular' | 'now_playing' | 'top_rated' | 'upcoming' | 'last_viewed';
+}
+
+export interface MovieInfo {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: any;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: { id: number; logo_path: string | null; name: string; origin_country: string }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: { english_name: string; iso_639_1: string; name: string }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
