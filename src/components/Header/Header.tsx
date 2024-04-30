1fad3db5dde7adfb4079383626b4bdd203c69dd9
hell-llex <demeshenko333@gmail.com>
2024-04-30 05:03:10 +0300

    feat: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
+import { useAppSelector } from '../../hooks/redux';
+
+const Header = ({ mainPage }: { mainPage: boolean }) => {
+  const navList = useAppSelector((state) => state.filmGuide.navItems);
+
+  return (
+    <>
+      <div className="header">
+        <a href="/" className="logo">
+          Film Guide
+        </a>
+        {mainPage && (<nav className="nav">
+          <ul className="nav-list">
+            {
+              navList?.map((elem) =>
+              (<li key={elem.name} className="nav-item">
+                <NavLink to={`/react-film-guide/${elem.link}`}>{elem.name}</NavLink>
+              </li>)
+              )
+            }
+          </ul>
+        </nav>)
+        }
+      </div >
+    </>
+  );
+};
+
+export default Header;
